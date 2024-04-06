71e9b02a4d89ca6c6395b10d033be8f43c133537
Jason Thomas <jason@openc3.com>
2024-04-06 14:51:43 -0600

    Update cli docs

--- openc3.sh
+++ openc3.sh
@@ -38,10 +38,10 @@ usage() {
   echo "Usage: $1 [cli, cliroot, start, stop, cleanup, run, util]" >&2
   echo "*  cli: run a cli command as the default user ('cli help' for more info)" 1>&2
   echo "*  cliroot: run a cli command as the root user ('cli help' for more info)" 1>&2
-  echo "*  start: start the docker compose openc3" >&2
-  echo "*  stop: stop the running dockers for openc3" >&2
-  echo "*  cleanup: cleanup network and volumes for openc3" >&2
-  echo "*  run: run the prebuilt containers for openc3" >&2
+  echo "*  start: alias for run" >&2
+  echo "*  stop: stop the containers (compose stop)" >&2
+  echo "*  cleanup: REMOVE volumes / data (compose down -v)" >&2
+  echo "*  run: run the containers (compose up)" >&2
   echo "*  util: various helper commands" >&2
   exit 1
 }
@@ -73,9 +73,6 @@ case $1 in
     docker run -it --rm --env-file "$(dirname -- "$0")/.env" --user=root --network openc3-cosmos-network -v `pwd`:/openc3/local:z -w /openc3/local $OPENC3_REGISTRY/$OPENC3_NAMESPACE/openc3-operator$OPENC3_IMAGE_SUFFIX:$OPENC3_TAG ruby /openc3/bin/openc3cli $args
     set +a
     ;;
-  start )
-    ${DOCKER_COMPOSE_COMMAND} -f compose.yaml up -d
-    ;;
   stop )
     ${DOCKER_COMPOSE_COMMAND} stop openc3-operator
     ${DOCKER_COMPOSE_COMMAND} stop openc3-cosmos-script-runner-api
@@ -97,8 +94,11 @@ case $1 in
       done
     fi
     ;;
-  run )
+  start | run )
     ${DOCKER_COMPOSE_COMMAND} -f compose.yaml up -d
+    ;;
+  run-ubi )
+    OPENC3_IMAGE_SUFFIX=-ubi OPENC3_REDIS_VOLUME=/home/data ${DOCKER_COMPOSE_COMMAND} -f compose.yaml up -d
     ;;
   util )
     set -a
