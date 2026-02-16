4fd43790361bd1e7c7f98f4b93dea93c65383e89
Jason Thomas <jason@openc3.com>
2026-02-16 14:18:45 -0700

    v7.0.0-rc1

--- scripts/linux/sync_openc3.sh
+++ scripts/linux/sync_openc3.sh
@@ -5,7 +5,7 @@ set -e
 echo "Usage: scripts/linux/sync_openc3.sh"
 
 cp ../cosmos/compose.yaml .
-cp ../cosmos/LICENSE.txt .
+cp ../cosmos/LICENSE.md .
 cp ../cosmos/cacert.pem .
 cp ../cosmos/openc3-redis/users.acl openc3-redis/.
 cp ../cosmos/openc3-traefik/traefik-allow-http.yaml openc3-traefik/.
@@ -14,10 +14,13 @@ cp ../cosmos/openc3-traefik/traefik-ssl.yaml openc3-traefik/.
 cp ../cosmos/openc3-traefik/traefik.yaml openc3-traefik/.
 cp ../cosmos/plugins/README.md plugins/.
 cp ../cosmos/plugins/DEFAULT/README.md plugins/DEFAULT/.
-cp ../cosmos/scripts/linux/openc3_util.sh scripts/linux/.
+cp ../cosmos/scripts/linux/benchmark_s3_migration.sh scripts/linux/.
+cp ../cosmos/scripts/linux/openc3_migrate_s3.sh scripts/linux/.
 cp ../cosmos/scripts/linux/openc3_upgrade.sh scripts/linux/.
-cp ../cosmos/scripts/windows/openc3_util.bat scripts/windows/.
+cp ../cosmos/scripts/linux/openc3_util.sh scripts/linux/.
+cp ../cosmos/scripts/windows/openc3_migrate_s3.bat scripts/windows/.
 cp ../cosmos/scripts/windows/openc3_upgrade.bat scripts/windows/.
+cp ../cosmos/scripts/windows/openc3_util.bat scripts/windows/.
 cp ../cosmos/.env .
 cp ../cosmos/openc3.sh .
 cp ../cosmos/openc3.bat .
